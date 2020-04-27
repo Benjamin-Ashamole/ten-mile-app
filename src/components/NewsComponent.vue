7c6f288d0191e3efc8ddda1272a01a529d335def
obiebryant <41204327+obiebryant@users.noreply.github.com>
2020-04-27 11:42:06 -0400

    Implementing the news page and button. Also registering the news page to the index.js router

---
+++ src/components/NewsComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>
+            <h1>Presenting Today's News</h1>
+        </div>
+        <div>
+            <button>News Button</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NewsButtonComponent"
+}
+</script>
+
+
+<style scoped>
+    
+</style>
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,14 +1,28 @@
 <template>
-    
+    <div class="news">
+        <news-button-component/>
+    </div>
 </template>
 
 <script>
+import NewsButtonComponent from "../components/NewsComponent.vue"
 export default {
-    
+    name: "News",
+    components: {
+        NewsButtonComponent
+    }
 }
 </script>
 
 
 <style scoped>
-
+.news {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    margin: auto;
+}
 </style>

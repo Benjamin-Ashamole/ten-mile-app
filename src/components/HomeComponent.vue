88ca7c7c96a65598e46930fb133df1dc2ac20e16
obiebryant <41204327+obiebryant@users.noreply.github.com>
2020-04-27 11:11:00 -0400

    implementing the home button component

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <div>
+        <h1> This is the Homepage</h1>
+    </div>
+    <div>
+        <button>Home Button</button>
+    </div>
+</div>
+</template>
+
+
+<script>
+export default {
+    name: "HomeButtonComponent"
+}
+</script>
+
+<style scoped>
+button {
+    background-color: rgb(65, 88, 189);
+    border-radius: 5px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
-    
+    <home-button-component/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import HomeButtonComponent from "../components/HomeComponent.vue"
 
 export default {
   name: "Home",
   components: {
+    HomeButtonComponent
   }
 };
 </script>
